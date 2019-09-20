1684e0ac02483031a8f4e9b8d7624c146750df0f
danial1021 <41097199+danial1021@users.noreply.github.com>
2019-09-20 20:27:14 +0900

    firts commit
    firts commit
    basic frame

---
+++ fe/src/views/corpor.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="article">
+    <div class="left">
+      <ul>
+        <li
+          :class="active == index && 'active'"
+          @click="active = index"
+          v-for="(x, index) in menulist"
+          :key="index"
+        >{{x}}</li>
+      </ul>
+    </div>
+    <div class="right">
+      <template v-if="active == 0">aa</template>
+      <template v-else-if="active == 1">bb</template>
+      <template v-else-if="active == 2">cc</template>
+      <template v-else>dd</template>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+      menulist: ["aaa", "bbbbb", "CCCCCCCCC ", "Dddddddddd"]
+    };
+  }
+};
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+}
+li {
+  padding: 0.41em 0.5em;
+  display: list-item;
+  text-align: -webkit-match-parent;
+  color: rgb(38, 133, 0);
+}
+.active {
+  background-color: rgb(38, 133, 0);
+  color: #fff;
+}
+.article {
+  min-height: 400px;
+  width: 980px;
+  margin: 0 auto;
+}
+.left {
+  display: block;
+  width: 200px;
+  float: left;
+}
+.right {
+  padding-top: 25px;
+  margin-left: 320px;
+  display: block;
+}
+</style>
---
+++ fe/src/views/edu_facility.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="article">
+    <pre>
+        <span style="font-size: 1.4em">JSON Web Tokens - jwt.io</span>
+
+https://jwt.io
+이 페이지 번역하기
+JSON Web Token (JWT) is a compact URL-safe means of representing claims to be transferred between two parties. The claims in a JWT are encoded as a ...
+[JWT] JSON Web Token 소개 및 구조 | VELOPERT.LOG
+
+https://velopert.com › ...
+2016. 12. 5. - 이번 포스트에서는, 토큰 기반 인증 시스템의 구현체인 JWT (JSON Web Token) 에 대한 소개를 하고, JWT 토큰의 기본 구조를 알아보도록 ...
+JWT(JSON Web Token)에 대해서... :: Outsider's Dev Story
+
+https://blog.outsider.ne.kr › ...
+2015. 7. 30. - 작년에 JSON Web Token(JWT)에 대한 글을 올렸다. 당시에는 JWT 처음 사용해 보면서 적은 글이라 그제야 약간 이해한 상황이었지만 시간이 지나 ...
+JWT (JSON Web Token) 이해하기와 활용 방안 - Opennaru, Inc.
+
+www.opennaru.com › opennaru-blog › jwt-json-web-token
+2018. 8. 16. - JWT 는 JSON Web Token의 약자로 전자 서명 된 URL-safe (URL로 이용할 수있는 문자 만 구성된)의 JSON입니다.JWT는 속성 정보 (Claim)를 ...</pre>
+  </div>
+</template>
+
+<style scoped>
+.article {
+  min-height: 400px;
+  padding: 5%;
+}
+</style>
